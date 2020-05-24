ff29479b9c5b7009bf239ce882c122d4e539e771
Dianaa <diana@timetree.io>
2020-05-24 16:46:07 +0300

    made this thing work

---
+++ src/bbs.js
@@ -0,0 +1,33 @@
+const p = 30091,
+  q = 400093; //numere prime
+const nr = p * q; //intreg Blum
+let x = 968670; //reziduu patratic modulo n
+
+// cmmdc a 2 numere
+const cmmdc = (a, b) => {
+  while (a !== b) {
+    if (a > b) {
+      a = a - b;
+    } else {
+      b = b - a;
+    }
+  }
+  return a;
+};
+
+// reziduu patratic modulo n. trebuie sa fie diferit de 0, 1, si sa nu ii aiba ca factori pe q sau p
+const generate = (s) => {
+  if (s !== 0 && s !== 1 && cmmdc(s, nr) === 1) {
+    //verifica ca numarul sa indeplineasca conditiile
+    x = s;
+    return s;
+  }
+};
+
+// genereaza urmatorul numar din secventa
+export const next = () => {
+  generate(x);
+  x = (x * x) % nr; //numarul generat
+  let b = x % 2; // bitul din secventa corespunzator
+  return b;
+};
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,142 +1,58 @@
 import React from "react";
-import {
-  TextField,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-  Button,
-} from "@material-ui/core";
-import { caesarCipher, bifidEncode, bifidDecode } from "../cripto";
+import { TextField, Button } from "@material-ui/core";
+import { next } from "../bbs";
 
 class Card extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "criptare",
-      hasFile: false,
-      key1: "",
-      key2: "",
+      size: 0,
     };
   }
-  fileReader;
 
-  //functia se apeleaza in momentul in care se apasa pe buton. se fac validarile pentru chei
-  handleCipherButton = (e) => {
-    const letters = /^[A-Za-z]+$/;
-    const numbers = /^[0-9]+$/;
-    const key1 = this.state.key1;
-    const key2 = this.state.key2;
-    if (!key1.match(numbers)) {
-      alert("Cheia pentru cifrul 1 trebuie sa fie un numar natural");
+  handleButton = (e) => {
+    let result = "";
+    if (!isNaN(this.state.size) && this.state.size > 0) {
+      //verifica daca dimensiunea este un numar > 0
+      for (let size = 0; size < this.state.size * 1024 * 1000; size++) {
+        result += next(); //gereaza cate un bit
+        console.log(size, this.state.size * 1024 * 1000);
+      }
+      console.log(result);
+      this.generateOutput(result);
+    } else {
+      alert("Introduceti un numar > 0");
       return;
     }
-    if (!key2.match(letters)) {
-      alert("Cheia pentru cifrul 2 trebuie sa contina doar litere");
-      return;
-    }
-    const content = this.fileReader.result;
-    let result;
-    if (this.state.value === "criptare") {
-      result = caesarCipher(content, parseInt(key1));
-      result = bifidEncode(result, key2);
-    } else {
-      result = bifidDecode(content, key2);
-      result = caesarCipher(result, 26 - parseInt(key1));
-    }
-    console.log(result);
-    this.generateOutput(result);
   };
 
-  //se verifica daca au fost introduse chei/fisier. butonul de criptare/decriptare este dezactivat daca nu sunt introduse toate datele
-  isValid = () => {
-    return this.state.hasFile && this.state.key1 && this.state.key2;
-  };
-
-  //se incarca fisierul .txt
-  handleFileChosen = (file) => {
-    this.fileReader = new FileReader();
-    if (file) {
-      this.fileReader.readAsText(file);
-      this.setState({ hasFile: true });
-    } else {
-      this.setState({ hasFile: false });
-    }
-  };
-
-  //se genereaza fisierul output.txt ce contine textul criptat/decriptat
+  //se genereaza fisierul output.bin ce contine secventa de numere
   generateOutput = (content) => {
+    const byteArray = Uint8Array.from(content); //transforma stringul in sir de bytes
     const element = document.createElement("a");
-    const file = new Blob([content], { type: "text/plain" });
+    const file = new Blob(byteArray, { type: "application/octet-stream" }); //scrie fisierul binar
     element.href = URL.createObjectURL(file);
-    element.download = "output.txt";
+    element.download = "output.bin";
     document.body.appendChild(element);
     element.click();
   };
 
+  //interfata
   render() {
     return (
       <div className="mainCard">
-        <div>
-          <div className="row cardText">Incarca fisier input(*.txt):</div>
-          <div>
-            <input
-              type="file"
-              id="file-input"
-              className="fileInput"
-              accept=".txt"
-              onChange={(e) => this.handleFileChosen(e.target.files[0])}
-            />
-          </div>
-        </div>
-
         <div className="row cardText">
-          Cheie cifrul Cezar:
+          Introduceti dimensiunea (MB):
           <div className="col-sm-4">
             <TextField
               id="standard-basic"
-              value={this.state.key1}
-              onChange={(e) => this.setState({ key1: e.target.value })}
+              value={this.state.size}
+              onChange={(e) => this.setState({ size: e.target.value })} //salveaza pe state dimensiunea introdusa
             />
           </div>
         </div>
-        <div className="row cardText">
-          Cheie cifrul bifid:
-          <div className="col-sm-4">
-            <TextField
-              id="standard-basic-2"
-              value={this.state.key2}
-              onChange={(e) => this.setState({ key2: e.target.value })}
-            />
-          </div>
-        </div>
-        <div className="row cardText">
-          <RadioGroup
-            aria-label="cripto"
-            name="cripto"
-            value={this.state.value}
-            onChange={(e) => {
-              this.setState({ value: e.target.value });
-            }}
-          >
-            <FormControlLabel
-              value="criptare"
-              control={<Radio color="primary" />}
-              label="Criptare"
-            />
-            <FormControlLabel
-              value="decriptare"
-              control={<Radio color="primary" />}
-              label="Decriptare"
-            />
-          </RadioGroup>
-        </div>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={this.handleCipherButton}
-          disabled={!this.isValid()}
-        >
-          {this.state.value === "criptare" ? "Cripteaza" : "Decripteaza"}
+        <Button variant="contained" color="primary" onClick={this.handleButton}>
+          Genereaza
         </Button>
       </div>
     );
